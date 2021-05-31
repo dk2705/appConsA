e80aed7371189af07ab46966e4a147fb8eca9eef
Dmytro Kotliar <dn270571kdb@gmail.com>
2021-06-01 00:13:47 +0300

    Init

---
+++ src/appconsa/AppConsA.java
@@ -0,0 +1,15 @@
+package appconsa;
+
+import java.util.Scanner;
+
+public class AppConsA {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String str = in.nextLine();
+        System.out.print("str="+str);
+    }
+    
+}
